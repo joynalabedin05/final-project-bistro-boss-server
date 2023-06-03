73f8f2355a4addf71816cf8199a2d83afb9b504f
joynalabedin05 <joynal05101993@gmail.com>
2023-06-03 11:46:06 +0600

    added

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
-require('dotenv').config()
+require('dotenv').config();
+
+const stripe = require('stripe')(process.env.PAYMENT_SECRET_KEY);
 
 const port = process.env.PORT || 5000;
 // MIDDELWARE
@@ -163,8 +165,21 @@ async function run() {
       const query  = {_id : new ObjectId(id)};
       const result = await cartCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
+    // create payment intent
+    app.post('/create-payment-intent',verifyJWT, async(req,res)=>{
+      const {price} = req.body;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      })
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
